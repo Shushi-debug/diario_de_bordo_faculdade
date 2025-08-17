f3c0613bdba74ce3b5b8f1cc00b540a9bc5bde58
Stefani <stefanimonteiroribeiro@hotmail.com>
2025-08-17 00:46:36 -0300

    Primeiro commit: projeto inicial do diario de bordo

---
+++ Semestre1/Laboratorio01/ExercicioAvancado_01.py
@@ -0,0 +1,12 @@
+a = float(input("Digite o primeiro nº: "))
+b = int(input("Digite o segundo nº: "))
+
+print(str(a),"multiplicado por",str(b),"é",str(a*b))
+
+print(str(a),"dividido por",str(b),"é",str(a/b))
+
+print(str(a),"mais",str(b),"é",str(a+b),"e",a,"-",b,"é",str(a-b))
+
+print(str(a),"elevado a",str(b),"é",(a**b))
+
+'''Exercícios de operações matemáticas'''
