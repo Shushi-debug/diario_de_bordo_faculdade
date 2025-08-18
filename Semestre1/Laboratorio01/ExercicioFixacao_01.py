27a03eb1f57114e713bd22c03a36cadd3a62fa2c
Stefani <stefanimonteiroribeiro@hotmail.com>
2025-08-17 23:33:03 -0300

    Newexercises

---
+++ Semestre1/Laboratorio01/ExercicioFixacao_01.py
@@ -0,0 +1,5 @@
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+altura = float(input("Digite sua altura: "))
+
+print(f"Olá {nome}! Você tem {idade} anos de idade e {altura} de altura!")
---
+++ Semestre1/Laboratorio01/ExercicioFixacao_02.py
@@ -0,0 +1,7 @@
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o primeiro número: "))
+divisao = num1/num2
+
+print(f"O primeiro resultado é {divisao:.2f}")
+print(f"O segundo resultado é {divisao:.3f}")
+print(f"O terceiro resultado é {divisao:.4f}")
